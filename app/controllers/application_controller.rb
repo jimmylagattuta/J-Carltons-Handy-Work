6787030972d81a16a2f9c5773bae88b0d21bc334
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-06-09 03:21:57 -0700

    rails configuration set up and ready for more, setting up react configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,58 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+	include ActionController::Cookies
+	before_action :authenticate_user, :cors_preflight_check
+	before_action :set_cors_headers
+	rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+	rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+	# after_filter :cors_set_access_control_headers
+	def cors_set_access_control_headers
+	  headers['Access-Control-Allow-Origin'] = '*'
+	  headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+	  headers['Access-Control-Allow-Headers'] = '*'
+	  headers['Access-Control-Max-Age'] = "1728000"
+	end
+
+	# If this is a preflight OPTIONS request, then short-circuit the
+	# request, return only the necessary headers and return an empty
+	# text/plain.
+
+	def cors_preflight_check
+	  if request.method == :options
+	    headers['Access-Control-Allow-Origin'] = '*'
+	    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+	    headers['Access-Control-Allow-Headers'] = '*'
+	    headers['Access-Control-Max-Age'] = '1728000'
+	    render :text => '', :content_type => 'text/plain'
+	  end
+	end
+	private
+	def set_cors_headers
+			response.set_header "Access-Control-Allow-Origin", origin
+	end
+
+	def origin
+			request.headers["Origin"] || "*"
+	end
+	def current_user
+		# @current_user = User.find_by_id(session[:user_id])
+	end
+
+	def record_not_found(errors)
+		# render json: errors.message, status: :not_found
+	end
+
+	def invalid_record(invalid)
+		# render json: invalid.record.errors, status: :unprocessable_entity
+	end
+
+	def authenticate_user
+		# puts "*" * 100
+		# puts "authenticate_user debugging"
+		# puts "current_user"
+		# puts current_user
+		# puts "current_user.inspect"
+		# puts current_user.inspect
+		# puts "*" * 100
+		# render json: "Not authorized", status: :unauthorized unless current_user
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "api/v1/home#hello"
+  # root "api/v1/home#hello"
     namespace :api do
-    namespace :v1 do
-      # get "/me", to: "users#show"
-      # post "/signup", to: "users#create"
-      # post "/login", to: "sessions#create"
-      # delete "/logout", to: "sessions#destroy"
+      namespace :v1 do
+        post "/signup", to: "users#create"
+        post "/login", to: "sessions#create"
+        delete "/logout", to: "sessions#destroy"
 
-    end
+      end
 
   end
     get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
