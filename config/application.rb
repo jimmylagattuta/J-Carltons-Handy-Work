99b4830de33337e285aceedbe26ad411764c3f55
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-06-09 02:11:09 -0700

    configuing rails

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,18 @@
 require_relative "boot"
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
-require "active_model/railtie"
+# require "active_model/railtie"
 # require "active_job/railtie"
-require "active_record/railtie"
+# require "active_record/railtie"
 # require "active_storage/engine"
-require "action_controller/railtie"
+# require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
+# require "action_view/railtie"
 # require "action_cable/engine"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -35,5 +35,9 @@ module JCarlton
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    # config.middleware.use Rack::Deflater
+    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "api/v1/home#hello"
+    namespace :api do
+    namespace :v1 do
+      # get "/me", to: "users#show"
+      # post "/signup", to: "users#create"
+      # post "/login", to: "sessions#create"
+      # delete "/logout", to: "sessions#destroy"
+
+    end
+
+  end
+    get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
