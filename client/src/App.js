b6edb02a63cfaee339c2691694f20e0dbc02241e
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-07-25 09:06:48 -0700

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Navbar />
         <h1>
           J. Carltons Handy Work
         </h1>
         <div id="under-construction">
           <h3>(...Under Construction...)</h3><i className="fas fa-wrench fa-2x"></i><i className="fas fa-hard-hat fa-2x"></i>
         </div>
-      </header>
     </div>
   );
 }
